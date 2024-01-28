995fa564cbac69e85e68fd5644e355653e2e0e37
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-01-28 16:12:05 +0200

    Changes: 1) changed the built in splash screen. 2) changed the background for the home page.

--- VoxelVisage/App.js
+++ VoxelVisage/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={["#A48559", "#02131B"]} style={styles.container}>
+
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
