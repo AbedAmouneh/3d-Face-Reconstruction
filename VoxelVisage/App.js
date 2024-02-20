fb7ce6d169c8c73a504dc762278ca7538542d5a2
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-02-21 01:03:56 +0200

    Added flash camera functionality

--- VoxelVisage/App.js
+++ VoxelVisage/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CameraScreen from "./src/components/Camera";
@@ -13,100 +13,70 @@ const CameraTabButton = ({ onPress }) => (
   <TouchableOpacity onPress={onPress} style={styles.cameraButton}>
     <Image
       source={require("./assets/camera.png")}
-      style={{ tintColor: "white", width: 30, height: 30 }}
+      style={{ tintColor: "white", width: 35, height: 35 }}
     />
   </TouchableOpacity>
 );
 
 export default function App() {
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  const handleRemoveImage = () => {
+    if (capturedImage) {
+      setCapturedImage(null);
+    } else {
+      Alert.alert("No Captured Image", "You have not captured an image.");
+    }
+  };
+
+  const handleCameraButtonPress = () => {};
+
   return (
-    <NavigationContainer
-      onStateChange={(state) => console.log("New state is", state)}
-    >
-      <LinearGradient colors={["#A48559", "#02131B"]} style={styles.container}>
-        <Tab.Navigator
-          initialRouteName="Camera"
-          screenOptions={{
-            tabBarStyle: [
-              {
+      <NavigationContainer>
+        <LinearGradient
+          colors={["#A48559", "#02131B"]}
+          style={styles.container}
+        >
+          <Tab.Navigator
+            initialRouteName="Camera"
+            screenOptions={{
+              tabBarStyle: {
                 backgroundColor: "black",
                 borderTopColor: "grey",
                 borderTopWidth: 1,
               },
-              null,
-            ],
-            tabBarShowLabel: false,
-            tabBarActiveTintColor: "white",
-          }}
-        >
-          <Tab.Screen
-            name="Settings"
-            component={SettingsScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Image
-                  source={require("./assets/settings-icon.png")}
-                  style={{ tintColor: color, width: size, height: size }}
-                />
-              ),
+              tabBarShowLabel: false,
+              tabBarActiveTintColor: "white",
             }}
-          />
-          <Tab.Screen
-            name="Close"
-            component={Screen}
-            initialParams={{ screenName: "Close" }}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Image
-                  source={require("./assets/close-icon.png")}
-                  style={{ tintColor: color, width: size, height: size }}
-                />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="Camera"
-            component={CameraScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Image
-                  source={require("./assets/add-icon.png")}
-                  style={{ tintColor: color, width: size, height: size }}
-                />
-              ),
-            }}
-          />
+          >
+            <Tab.Screen
+              name="Settings"
+              component={SettingsScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Image
+                    source={require("./assets/settings-icon.png")}
+                    style={{ tintColor: color, width: size, height: size }}
+                  />
+                ),
+              }}
+            />
 
-          <Tab.Screen
-            name="Download"
-            component={Screen}
-            initialParams={{ screenName: "Download" }}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Image
-                  source={require("./assets/download-icon.png")}
-                  style={{ tintColor: color, width: size, height: size }}
-                />
-              ),
-            }}
-          />
 
-          <Tab.Screen
-            name="Save"
-            component={Screen}
-            initialParams={{ screenName: "Save" }}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Image
-                  source={require("./assets/save-icon.png")}
-                  style={{ tintColor: color, width: size, height: size }}
-                />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </LinearGradient>
-    </NavigationContainer>
+            <Tab.Screen
+              name="Camera"
+              component={CameraScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <CameraTabButton onPress={handleCameraButtonPress} />
+                ),
+              }}
+            />
+
+
+          </Tab.Navigator>
+        </LinearGradient>
+      </NavigationContainer>
   );
 }
 
--- VoxelVisage/src/components/Camera.jsx
+++ VoxelVisage/src/components/Camera.jsx
@@ -11,16 +11,17 @@ import {
 import { Camera } from "expo-camera";
 import * as ImagePicker from "expo-image-picker";
 
-const CameraScreen = ({ navigation }) => {
+const CameraScreen = React.memo(() => {
+  const [capturedImage, setCapturedImage] = useState(null);
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
-  const [capturedImage, setCapturedImage] = useState(null);
   const [focusing, setFocusing] = useState(false);
   const [animation] = useState(new Animated.Value(0));
+  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
   const cameraRef = useRef(null);
 
   useEffect(() => {
-    (async () => {
+    const checkPermissions = async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === "granted");
 
@@ -29,18 +30,28 @@ const CameraScreen = ({ navigation }) => {
       if (galleryStatus.status !== "granted") {
         alert("Sorry, we need camera roll permissions to make this work!");
       }
-    })();
-  }, []);
+    };
+
+    checkPermissions();
+
+    return () => {
+      animation.removeAllListeners();
+    };
+  }, [animation]);
 
   const takePicture = async () => {
     if (cameraRef.current) {
       setFocusing(true);
+
       Animated.timing(animation, {
         toValue: 1,
         duration: 1000,
         easing: Easing.linear,
         useNativeDriver: false,
-      }).start();
+      }).start(() => {
+        animation.setValue(0);
+      });
+
       const options = { quality: 0.5, base64: true };
       const data = await cameraRef.current.takePictureAsync(options);
 
@@ -62,6 +73,14 @@ const CameraScreen = ({ navigation }) => {
     }
   };
 
+  const toggleFlash = () => {
+    setFlashMode(
+      flashMode === Camera.Constants.FlashMode.off
+        ? Camera.Constants.FlashMode.on
+        : Camera.Constants.FlashMode.off
+    );
+  };
+
   if (hasPermission === null) {
     return <View />;
   }
@@ -70,7 +89,7 @@ const CameraScreen = ({ navigation }) => {
   }
 
   const { width, height } = Dimensions.get("window");
-  const imageSize = { width: width * 1, height: height * 0.9}; 
+  const imageSize = { width: width * 1, height: height * 0.9 };
 
   const circleSize = animation.interpolate({
     inputRange: [0, 0.5, 1],
@@ -79,18 +98,25 @@ const CameraScreen = ({ navigation }) => {
 
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
+      <Camera
+        style={{ flex: 1 }}
+        type={type}
+        ref={cameraRef}
+        flashMode={flashMode}
+      >
         <View
           style={{
             flex: 1,
             backgroundColor: "transparent",
             flexDirection: "row",
+            justifyContent: "space-between",
           }}
         >
           <TouchableOpacity
             style={{
-              flex: 0.1,
-              alignSelf: "flex-end",
+              position: "absolute",
+              bottom: 16,
+              left: 16,
               alignItems: "center",
               zIndex: 4,
             }}
@@ -109,16 +135,17 @@ const CameraScreen = ({ navigation }) => {
           </TouchableOpacity>
           <TouchableOpacity
             style={{
-              flex: 1,
-              alignSelf: "flex-end",
-              alignItems: "center",
+              position: "absolute",
+              bottom: 16,
+              left: "50%",
+              transform: [{ translateX: -28 }], 
               zIndex: 2,
             }}
             onPress={takePicture}
           >
             <Image
               source={require("../../assets/camera.png")}
-              style={{ tintColor: "white", width: 60, height: 60 }}
+              style={{ tintColor: "white", width: 55, height: 55 }}
             />
             {focusing && (
               <Animated.View
@@ -140,8 +167,9 @@ const CameraScreen = ({ navigation }) => {
           </TouchableOpacity>
           <TouchableOpacity
             style={{
-              flex: 0.1,
-              alignSelf: "flex-end",
+              position: "absolute",
+              bottom: 16,
+              right: 16,
               alignItems: "center",
               zIndex: 2,
             }}
@@ -149,6 +177,27 @@ const CameraScreen = ({ navigation }) => {
           >
             <Image
               source={require("../../assets/gallery-icon.png")}
+              style={{ tintColor: "white", width: 30, height: 30 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: "flex-start",
+              alignItems: "center",
+              zIndex: 2,
+              marginLeft: "auto",
+              marginRight: 14,
+              marginTop: 16,
+            }}
+            onPress={toggleFlash}
+          >
+            <Image
+              source={
+                flashMode === Camera.Constants.FlashMode.off
+                  ? require("../../assets/flash-off.png")
+                  : require("../../assets/flash-on.png")
+              }
               style={{ tintColor: "white", width: 30, height: 30 }}
             />
           </TouchableOpacity>
@@ -167,14 +216,23 @@ const CameraScreen = ({ navigation }) => {
           }}
         >
           <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
           >
             <Image source={{ uri: capturedImage.uri }} style={imageSize} />
+            <TouchableOpacity onPress={() => setCapturedImage(null)}>
+              <Text style={{ color: "white", marginTop: 10 }}>
+                Remove Image
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
       )}
     </View>
   );
-};
+});
 
 export default CameraScreen;
