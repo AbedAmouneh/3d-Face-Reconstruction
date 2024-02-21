c84b4157a4dabb678d5eba2374583cd1e4a9d500
farhatrkhalil <khalilrfarhat297@gmail.com>
2024-02-21 21:46:01 +0200

    fixed a bug related to gallery

--- VoxelVisage/src/components/Camera.jsx
+++ VoxelVisage/src/components/Camera.jsx
@@ -27,6 +27,7 @@ const CameraScreen = React.memo(() => {
   const [animation] = useState(new Animated.Value(0));
   const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
   const cameraRef = useRef(null);
+  const [refreshKey, setRefreshKey] = useState(0);
 
   useEffect(() => {
     const checkPermissions = async () => {
@@ -100,15 +101,24 @@ const CameraScreen = React.memo(() => {
   };
 
   const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
 
-    if (!result.cancelled) {
-      setCapturedImage({ uri: result.uri });
+      if (!result.cancelled) {
+        setCapturedImage(result);
+      } else {
+        // User cancelled image selection, trigger a refresh
+        setRefreshKey((prevKey) => prevKey + 1);
+      }
+    } catch (error) {
+      // Display an alert with the error message
+      Alert.alert("Error", `Error selecting image: ${error.message}`);
+      console.error("Error selecting image:", error);
     }
   };
 
@@ -121,6 +131,9 @@ const CameraScreen = React.memo(() => {
   };
 
   const handleLongPress = async () => {
+    if (!capturedImage) {
+      return;
+    }
     let successMessageDisplayed = false;
 
     Alert.alert(
@@ -229,8 +242,154 @@ const CameraScreen = React.memo(() => {
     },
   });
 
+  const renderCameraButtons = () => (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "flex-end",
+        marginBottom: 16,
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          bottom: 16,
+          left: 16,
+          alignItems: "center",
+          zIndex: 2,
+          ...shadowStyle,
+        }}
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      >
+        <Image
+          source={require("../../assets/flip-camera-icon.png")}
+          style={{ tintColor: "white", width: 30, height: 30 }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          bottom: 16,
+          left: "50%",
+          transform: [{ translateX: -28 }],
+          zIndex: 2,
+          ...shadowStyle,
+        }}
+        onPress={takePicture}
+      >
+        <Image
+          source={require("../../assets/Shutter.png")}
+          style={{ tintColor: "white", width: 65, height: 65 }}
+        />
+        {focusing && (
+          <Animated.View
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              backgroundColor: "transparent",
+              borderColor: "white",
+              borderWidth: 2,
+              borderRadius: 25,
+              width: 50,
+              height: 50,
+              transform: [
+                { translateX: -25 },
+                { translateY: -25 },
+                { rotate: circleRotation },
+              ],
+              zIndex: 3,
+            }}
+          />
+        )}
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          bottom: 16,
+          right: 16,
+          alignItems: "center",
+          zIndex: 2,
+          ...shadowStyle,
+        }}
+        onPress={pickImage}
+      >
+        <Image
+          source={require("../../assets/gallery-icon.png")}
+          style={{ tintColor: "white", width: 30, height: 30 }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          flex: 0.1,
+          alignSelf: "flex-start",
+          alignItems: "center",
+          zIndex: 2,
+          marginLeft: "auto",
+          marginRight: 8,
+          marginTop: 16,
+          ...shadowStyle,
+        }}
+        onPress={toggleFlash}
+      >
+        <Image
+          source={
+            flashMode === Camera.Constants.FlashMode.off
+              ? require("../../assets/flash-off.png")
+              : require("../../assets/flash-on.png")
+          }
+          style={{ tintColor: "white", width: 30, height: 30 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const renderCapturedImage = () => (
+    <View
+      style={{
+        flex: 1,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 2,
+      }}
+    >
+      <LongPressGestureHandler
+        onHandlerStateChange={({ nativeEvent }) => {
+          if (nativeEvent.state === State.ACTIVE) {
+            handleLongPress();
+          }
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 2,
+          }}
+        >
+          <Image source={{ uri: capturedImage.uri }} style={imageSize} />
+          <TouchableOpacity onPress={() => setCapturedImage(null)}>
+            <Text style={{ color: "white", marginTop: 10 }}>Remove Image</Text>
+          </TouchableOpacity>
+        </View>
+      </LongPressGestureHandler>
+    </View>
+  );
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView key={refreshKey.toString()} style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
         <Camera
           style={{ flex: 1 }}
@@ -242,152 +401,8 @@ const CameraScreen = React.memo(() => {
             handleFocus({ x, y });
           }}
         >
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: "transparent",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "flex-end",
-              marginBottom: 16,
-            }}
-          >
-            <TouchableOpacity
-              style={{
-                position: "absolute",
-                bottom: 16,
-                left: 16,
-                alignItems: "center",
-                zIndex: 2,
-                ...shadowStyle,
-              }}
-              onPress={() => {
-                setType(
-                  type === Camera.Constants.Type.back
-                    ? Camera.Constants.Type.front
-                    : Camera.Constants.Type.back
-                );
-              }}
-            >
-              <Image
-                source={require("../../assets/flip-camera-icon.png")}
-                style={{ tintColor: "white", width: 30, height: 30 }}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                position: "absolute",
-                bottom: 16,
-                left: "50%",
-                transform: [{ translateX: -28 }],
-                zIndex: 2,
-                ...shadowStyle,
-              }}
-              onPress={takePicture}
-            >
-              <Image
-                source={require("../../assets/Shutter.png")}
-                style={{ tintColor: "white", width: 65, height: 65 }}
-              />
-              {focusing && (
-                <Animated.View
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    backgroundColor: "transparent",
-                    borderColor: "white",
-                    borderWidth: 2,
-                    borderRadius: 25,
-                    width: 50,
-                    height: 50,
-                    transform: [
-                      { translateX: -25 },
-                      { translateY: -25 },
-                      { rotate: circleRotation },
-                    ],
-                    zIndex: 3,
-                  }}
-                />
-              )}
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                position: "absolute",
-                bottom: 16,
-                right: 16,
-                alignItems: "center",
-                zIndex: 2,
-                ...shadowStyle,
-              }}
-              onPress={pickImage}
-            >
-              <Image
-                source={require("../../assets/gallery-icon.png")}
-                style={{ tintColor: "white", width: 30, height: 30 }}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                flex: 0.1,
-                alignSelf: "flex-start",
-                alignItems: "center",
-                zIndex: 2,
-                marginLeft: "auto",
-                marginRight: 8,
-                marginTop: 16,
-                ...shadowStyle,
-              }}
-              onPress={toggleFlash}
-            >
-              <Image
-                source={
-                  flashMode === Camera.Constants.FlashMode.off
-                    ? require("../../assets/flash-off.png")
-                    : require("../../assets/flash-on.png")
-                }
-                style={{ tintColor: "white", width: 30, height: 30 }}
-              />
-            </TouchableOpacity>
-          </View>
+          {capturedImage ? renderCapturedImage() : renderCameraButtons()}
         </Camera>
-        {capturedImage && (
-          <View
-            style={{
-              flex: 1,
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              zIndex: 2,
-            }}
-          >
-            <LongPressGestureHandler
-              onHandlerStateChange={({ nativeEvent }) => {
-                if (nativeEvent.state === State.ACTIVE) {
-                  handleLongPress();
-                }
-              }}
-            >
-              <View
-                style={{
-                  flex: 1,
-                  justifyContent: "center",
-                  alignItems: "center",
-                  zIndex: 2,
-                }}
-              >
-                <Image source={{ uri: capturedImage.uri }} style={imageSize} />
-                <TouchableOpacity onPress={() => setCapturedImage(null)}>
-                  <Text style={{ color: "white", marginTop: 10 }}>
-                    Remove Image
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </LongPressGestureHandler>
-          </View>
-        )}
       </View>
     </GestureHandlerRootView>
   );
